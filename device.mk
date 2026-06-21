45e36d882b61f8507a990ad14b14d4a0939ceb0f
Аезер <aetheraylia@gmail.com>
2026-06-21 23:21:16 +0300

    Add device flags from twrp_taipei.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,12 @@
 
 # Inherit from mt6855-common
 $(call inherit-product, device/motorola/taipei/common.mk)
+
+# Product Specifics
+PRODUCT_NAME := twrp_taipei
+PRODUCT_DEVICE := taipei
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := taipei
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-taipei
