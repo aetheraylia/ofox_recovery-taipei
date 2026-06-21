e20178e47521ec5e130c3ff036c231f0de64c605
Аезер <aetheraylia@gmail.com>
2026-06-21 23:13:24 +0300

    Remove noused partitions

--- common.mk
+++ common.mk
@@ -25,13 +25,10 @@ AB_OTA_PARTITIONS += \
     boot \
     dtbo \
     lk \
-    odm \
-    odm_dlkm \
     product \
     system \
     system_ext \
     vbmeta_system \
-    vbmeta_vendor \
     vendor \
     vendor_boot \
     vendor_dlkm
