b316c8826b81649e0c7d4252dc3dab44a4638862
Аезер <aetheraylia@gmail.com>
2026-07-27 19:22:17 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -22,32 +22,32 @@ ENABLE_VIRTUAL_AB := true
 AB_OTA_UPDATER := true
 
 AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    lk \
-    product \
-    system \
-    system_ext \
-    vbmeta_system \
-    vendor \
-    vendor_boot \
-    vendor_dlkm
+boot \
+dtbo \
+lk \
+product \
+system \
+system_ext \
+vbmeta_system \
+vendor \
+vendor_boot \
+vendor_dlkm
 
 AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+RUN_POSTINSTALL_system=true \
+POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
+FILESYSTEM_TYPE_system=ext4 \
+POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=ext4 \
-    POSTINSTALL_OPTIONAL_vendor=true
+RUN_POSTINSTALL_vendor=true \
+POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+FILESYSTEM_TYPE_vendor=ext4 \
+POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh
+otapreopt_script \
+cppreopts.sh
 
 PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
 
@@ -60,40 +60,44 @@ PRODUCT_TARGET_VNDK_VERSION := 31
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
+android.hardware.boot@1.2-mtkimpl \
+android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
-    bootctl
+bootctl
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+android.hardware.fastboot@1.0-impl-mock \
+fastbootd
 
 # Health Hal
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+android.hardware.health@2.1-impl \
+android.hardware.health@2.1-service
+
+# Keystore Hal
+PRODUCT_PACKAGES += \
+    android.system.keystore2
 
 # MTK plpath utils
 PRODUCT_PACKAGES += \
-    mtk_plpath_utils.recovery
+mtk_plpath_utils.recovery
 
 # Security
 PRODUCT_PACKAGES += \
-    android.hardware.security.keymint \
-    android.hardware.security.secureclock \
-    android.hardware.security.sharedsecret
+android.hardware.security.keymint \
+android.hardware.security.secureclock \
+android.hardware.security.sharedsecret
 
 # Update engine
 PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
+update_engine \
+update_engine_sideload \
+update_verifier
 
 PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
+update_engine_client
 
 # Product Specifics
 PRODUCT_NAME := twrp_taipei
